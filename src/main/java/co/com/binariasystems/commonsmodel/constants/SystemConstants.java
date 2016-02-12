187192a9c04d5d44f33d8089f6052d4aa40a8087
Alexander Castro Olivo <alexander.castro@techandsolve.com>
2016-02-12 16:45:49 -0500

    refactorizacion

---
+++ src/main/java/co/com/binariasystems/commonsmodel/constants/SystemConstants.java
@@ -0,0 +1,10 @@
+package co.com.binariasystems.commonsmodel.constants;
+
+public interface SystemConstants {
+	final String APP_NAME_PROP  = "SystemEnvironment.applicationName";
+	final String APP_VERSION_PROP  = "SystemEnvironment.applicationVersion";
+	final String MAIN_DSOURCE_PROP = "SystemEnvironment.mainDataSourceName";
+	final String ORION_RESTAPI_LOGIN_PARAM = "ORION_RESTAPI_LOGINALIAS";
+	final String ORION_RESTAPI_PWD_PARAM = "ORION_RESTAPI_PASSWORD";
+	final String ORION_RESTAPI_URL_PARAM = "ORION_RESTAPI_BASEURL";
+}
