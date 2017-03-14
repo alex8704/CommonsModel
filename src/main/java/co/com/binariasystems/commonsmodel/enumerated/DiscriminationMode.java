13ff4e305532c339f403ba4d45c98be5ba67e0d1
Alexander Castro Olivo <alexander.castro@techsolve.com>
2017-03-14 13:39:10 -0500

    DiscriminationMode

--- src/main/java/co/com/binariasystems/commonsmodel/enumerated/DiscriminationMode.java
+++ src/main/java/co/com/binariasystems/commonsmodel/enumerated/DiscriminationMode.java
@@ -1,6 +1,24 @@
 package co.com.binariasystems.commonsmodel.enumerated;
 
 public enum DiscriminationMode {
-	private float firstTimePercent;
-	private float lastTimePercent;
+	ZERO_HUNDRED(0,1),
+	FIFTY_FIFTY(0.5f,0.5f),
+	HUNDRED_ZERO(1f,0);
+	private final float firstTimePercent;
+	private final float lastTimePercent;
+	
+	private DiscriminationMode(float firstTimePercent, float lastTimePercent) {
+		this.firstTimePercent = firstTimePercent;
+		this.lastTimePercent = lastTimePercent;
+	}
+
+	public float getFirstTimePercent() {
+		return firstTimePercent;
+	}
+
+	public float getLastTimePercent() {
+		return lastTimePercent;
+	}
+	
+	
 }
